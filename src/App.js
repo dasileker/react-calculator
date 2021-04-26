38305b865cc0dbc7db8e686a55ded3e34f0ac6dd
zerradi <dasileker@gmail.com>
2021-04-26 15:19:04 +0000

    created the Milestone2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-// import Nav from './Nav';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Nav /> */}
         <h1>React-calculator</h1>
       </header>
     </div>
