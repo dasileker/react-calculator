ddbb15695e02dc762fb97c2ab9945e3fa07e8f17
zerradi <dasileker@gmail.com>
2021-04-27 14:23:53 +0000

    refactored the codes + files

--- src/components/Btn.js
+++
@@ -1,9 +0,0 @@
-import propTypes from 'prop-types';
-
-const { BTn } = btn => <button type="button">{btn.key}</button>;
-
-export default BTn;
-
-BTn.propTypes = {
-  key: propTypes.string,
-}
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,13 +1,42 @@
-import BTn from './Btn';
+import React from 'react';
+import Button from './Button';
 
-const ButtonPanel = () => {
-  <section>
+const ButtonPanel = () => (
+  <>
     <div>
-      <BTn key="1" />
-      <BTn key="5" />
-      <BTn key="6" />
+      <Button name="AC" />
+      <Button name="+/-" />
+      <Button name="%" />
+      <Button name="÷" />
     </div>
-  </section>
-}
+
+    <div>
+      <Button name="7" />
+      <Button name="8" />
+      <Button name="9" />
+      <Button name="X" />
+    </div>
+
+    <div>
+      <Button name="4" />
+      <Button name="5" />
+      <Button name="6" />
+      <Button name="-" />
+    </div>
+
+    <div>
+      <Button name="1" />
+      <Button name="2" />
+      <Button name="3" />
+      <Button name="+" />
+    </div>
+
+    <div>
+      <Button name="0" />
+      <Button name="." />
+      <Button name="=" />
+    </div>
+  </>
+  );
 
 export default ButtonPanel;
