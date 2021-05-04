b3394d57309c23e2069f9bbd8090958a40821fdb
zerradi <dasileker@gmail.com>
2021-05-04 12:36:12 +0000

    add calculate.js

---
+++ src/logic/Calculate.js
@@ -0,0 +1,8 @@
+import { number } from 'prop-types';
+import operate from './Operate';
+
+const isOperation = op => ['X', '-', '+', '÷', '+/-', '%'].indexOf(op) > -1;
+
+const appendNum = (data, number) => {
+  
+}
