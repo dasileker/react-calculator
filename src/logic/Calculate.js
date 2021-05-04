6e982e75e8dca20844aac21da87fad68e0a52783
zerradi <dasileker@gmail.com>
2021-05-04 12:48:58 +0000

    add logic to  calculate

--- src/logic/Calculate.js
+++ src/logic/Calculate.js
@@ -4,5 +4,19 @@ import operate from './Operate';
 const isOperation = op => ['X', '-', '+', '÷', '+/-', '%'].indexOf(op) > -1;
 
 const appendNum = (data, number) => {
-  
+  if (!data) {
+    return number;
+  }
+
+  if (
+    (data === '0' && number === '0') || (number == '.' && data.indexOf('.') > -1)
+  ) {
+    return data;
+  }
+
+  return `${data}${number}`
+};
+
+const calculate = ({ tootal, next, operation }, button) => {
+  if 
 }
