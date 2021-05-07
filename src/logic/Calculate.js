91703f0653c8df3ec302584f68e3ec9416985a86
zerradi <dasileker@gmail.com>
2021-05-07 16:00:32 +0000

    refactored the code

--- src/logic/Calculate.js
+++ src/logic/Calculate.js
@@ -7,48 +7,57 @@ const appendNum = (data, number) => {
     return number;
   }
 
-  if ((data === '0' && number === '0') || (number === '.' && data.indexOf('.') > -1)) {
+  if (
+    (data === '0' && number === '0')
+    || (number === '.' && data.indexOf('.') > -1)
+  ) {
     return data;
   }
 
   return `${data}${number}`;
 };
 
-const calculate = ({ total, next, operation }, button) => {
+const calculate = ({ total = null, next = null, operation = null }, button) => {
   if (button === 'AC') {
-    return {};
+    return { total: null, next: null, operation: null };
   } if (button === '=') {
     if (next && operation && total) {
-      return { next: operate(next, total, operation) };
+      return { total: operate(next, total, operation), operation: null, next: null };
     }
   } else if (isOperation(button)) {
     if (button === '+/-') {
       if (total) {
         return { operation, next, total: operate(total, '-1', 'X') };
       } if (next) {
-        return { next: operate(next, '-1', 'X') };
+        return { next: operate(next, '-1', 'X'), total: null, operation: null };
       }
     } else if (button === '%') {
       if (total) {
         return { next, operation, total: operate(next, operate(total, '100', '÷'), 'X') };
       }
-    } else {
-      if (total) {
-        return {
-          next: operate(next, total, operation),
-          operation: button,
-        };
-      } if (next) {
-        return {
-          next,
-          operation: button,
-        };
-      }
+    } else if (total && operation && next) {
+      return {
+        next: operate(next, total, operation),
+        operation: button,
+        total: null,
+      };
+    } else if (total && !next) {
+      return {
+        next: total,
+        operation: button,
+        total: null,
+      };
+    } else if (next) {
+      return {
+        next,
+        operation: button,
+        total: null,
+      };
     }
   } else if (operation) {
     return { total: appendNum(total, button), next, operation };
   } else if (next || button !== '0') {
-    return { next: appendNum(next, button), total, operation };
+    return { next: appendNum(next, button), total: null, operation: null };
   }
 
   return { total, next, operation };
