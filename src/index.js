2833656f68c61826d4296d733f4af3f64c21f6bc
zerradi <dasileker@gmail.com>
2021-05-24 15:04:29 +0100

    add the build mode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter } from 'react-router-dom';
 import Routes from './Components/Routes';
-// import App from './Components/App';
-// import NavBar from './Components/Nav';
-// import Home from './Components/Home';
-// import Quote from './Components/Quote';
 
 import './index.css';
 
