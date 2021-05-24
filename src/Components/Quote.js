4721df02c3ff35cba7cefabd9258da3988cea100
zerradi <dasileker@gmail.com>
2021-05-24 15:53:05 +0100

    fix linters errors

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-// eslint-disable-length
+
 const Quote = () => (
   <>
     <div className="calculator">
       <h1 className="quote">Quote of the day</h1>
       <p className="day-quote">
         <q>
-          Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers.
+          Without mathematics, Everything around you is numbers.
           <span>Albert Einstein</span>
         </q>
       </p>
