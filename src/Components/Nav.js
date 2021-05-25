5784f6aef88347f29afd969d9806d81b62fdd1cc
zerradi <dasileker@gmail.com>
2021-05-25 13:23:47 +0100

    fix linters errors

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,6 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
-<<<<<<< HEAD
 // import Home from './Home';
-=======
->>>>>>> 47348e66858a86e9e984fbcaebd62c444ac4abac
 import './App.css';
 
 const NavBar = () => (
@@ -13,11 +10,8 @@ const NavBar = () => (
       <ul>
         <Link to="./home"><li>Home</li></Link>
         <Link to="./app"><li>Calculator</li></Link>
-<<<<<<< HEAD
-        <Link to="./Quote"><li>Quote</li></Link>
-=======
+
         <Link to="/quote"><li>Quote</li></Link>
->>>>>>> 47348e66858a86e9e984fbcaebd62c444ac4abac
       </ul>
 
     </div>
