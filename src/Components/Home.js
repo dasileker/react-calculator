da6ef7d717d59f07d4021300ad87d318ac193f80
zerradi <dasileker@gmail.com>
2021-05-25 15:26:41 +0100

    Revert "correcting style erros"
    This reverts commit 621d465c331fea15fc63d2945cfcf63e9bf94a3a.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,15 +7,17 @@ const Home = () => (
     {/* <div className="home-title">
       <NavBar />
     </div> */}
-    <h2 className="title">Welcome to Math-Magicians</h2>
-    <div className="text">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-      aliquip ex ea commodo consequat. Duis aute irure dolor in
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-      culpa qui o!cia deserunt mollit anim id est laborum.
+    <div className="calculator">
+      <h2 className="title">Welcome to Math-Magicians</h2>
+      <div className="text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui o!cia deserunt mollit anim id est laborum.
+      </div>
     </div>
   </>
 );
